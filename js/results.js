c7082df8f8e11376a4009df20f993f2736aa6567
mobs73 <mobs73@outlook.com>
2024-07-05 16:21:29 +0900

    프로젝트 최초 생성

---
+++ js/results.js
@@ -0,0 +1,26 @@
+import { results, mbtis } from '/js/data.js'
+
+const mbti = new URLSearchParams(location.search).get('mbti')
+console.log(mbti)
+const result = results[mbtis[mbti]]
+
+const titleEl = document.querySelector('.page-title')
+const characterEL = document.querySelector('.character')
+const boxEls = document.querySelectorAll('.box')
+const jobEls = document.querySelectorAll('.job')
+const lectureEl = document .querySelector('.lecture')
+const lectureImgEl = document .querySelector('.lecture img')
+
+titleEl.innerHTML = result.title
+characterEL.src = result.character
+
+boxEls.forEach(function (boxEl, index) {
+    boxEl.innerHTML = result.results[index]
+})
+
+jobEls.forEach(function (jobEl, index) {
+    jobEl.innerHTML = result.jobs[index]
+})
+
+lectureEl.href = result.lectureUrl
+lectureImgEl.src = result.lectureImg
